37563dbec0dc04f0fcc9a417a97aad4584f02844
FishMoies <xisiyao0529@gmail.com>
2026-04-22 20:33:34 +0800

    添加部分注释

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   // 使用 Vue 插件
   plugins: [vue()],
-  // 部署基础路径（用于 GitHub Pages）
+  // 部署基础路径（用于宝塔自动部署/如果需要更新github page，请改成/PersonalWebsite/，然后上传）
   base: '/',
 })
